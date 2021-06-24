ad41ff1fa0c98138d40c88d66bf0f566b4b2903e
Anderson Hansen <adshan@gmail.com>
2021-06-24 17:35:06 -0300

    Criação do projeto

---
+++ MessagingRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MessagingRabbitMQ.Consumer
+{
+    class Program
+    {
+        private const string QUEUE = "my-queue";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var byteArray = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(byteArray);
+
+                Console.WriteLine(Environment.NewLine + "[MESSAGE RECEIVED] " + message);
+
+                channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(QUEUE, false, consumer);
+
+            Console.WriteLine("Waiting new messages...Press some key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessagingRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MessagingRabbitMQ.Publisher
+{
+    class Program
+    {
+        private const string QUEUE = "my-queue";
+        private const string EXCHANGE = "my-exchange";
+
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+
+            while (true)
+            {
+                Console.WriteLine("Write your message: ");
+
+                var message = Console.ReadLine();
+
+                string messageToPublish = $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " + $"{message}";
+
+                var body = Encoding.UTF8.GetBytes(messageToPublish);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: QUEUE,
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
